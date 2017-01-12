995587c3d52073dfda47cc1300aa77f5ca0dc109
Russell Gordon <rgordon@rsgc.on.ca>
2017-01-12 14:11:55 -0500

    Reads input file and prints output to the terminal

--- MentalHealthWeek/main.swift
+++ MentalHealthWeek/main.swift
@@ -8,5 +8,50 @@
 
 import Foundation
 
-print("Hello, World!")
+/// Read text file line by line
+class LineReader {
+    let path: String
+    
+    fileprivate let file: UnsafeMutablePointer<FILE>!
+    
+    init?(path: String) {
+        self.path = path
+        
+        file = fopen(path, "r")
+        
+        guard file != nil else { return nil }
+        
+    }
+    
+    var nextLine: String? {
+        var line:UnsafeMutablePointer<CChar>? = nil
+        var linecap:Int = 0
+        defer { free(line) }
+        return getline(&line, &linecap, file) > 0 ? String(cString: line!) : nil
+    }
+    
+    deinit {
+        fclose(file)
+    }
+}
+
+extension LineReader: Sequence {
+    func  makeIterator() -> AnyIterator<String> {
+        return AnyIterator<String> {
+            return self.nextLine
+        }
+    }
+}
+
+// Read the text file (place in your home folder)
+// Path will probably be /Users/student/survey_response_sample.txt
+// Obtain the data file on Haiku, Day 37
+guard let reader = LineReader(path: "/Users/russellgordon/survey_response_sample.txt") else {
+    exit(0); // cannot open file
+}
+
+// Iterate over each line in the file and print to the terminal
+for line in reader {
+    print(">" + line.trimmingCharacters(in: .whitespacesAndNewlines))
+}
 
