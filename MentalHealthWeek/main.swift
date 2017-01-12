59183c2a06dc3a20cd85aaa3b59d62ae978d96fa
Russell Gordon <rgordon@rsgc.on.ca>
2017-01-12 12:50:32 -0500

    Initial Commit

---
+++ MentalHealthWeek/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MentalHealthWeek
+//
+//  Created by Russell Gordon on 1/12/17.
+//  Copyright © 2017 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
