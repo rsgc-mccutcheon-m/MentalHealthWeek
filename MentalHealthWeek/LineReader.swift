e900955700a8e9ff62d6c71e5d742ea26899a4df
Russell Gordon <rgordon@rsgc.on.ca>
2017-01-13 14:18:29 -0500

    Moved file reading code into a separate file.

---
+++ MentalHealthWeek/LineReader.swift
@@ -0,0 +1,44 @@
+//
+//  LineReader.swift
+//  MentalHealthWeek
+//
+//  Created by Russell Gordon on 1/13/17.
+//  Copyright © 2017 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+/// Read text file line by line
+class LineReader {
+    let path: String
+    
+    fileprivate let file: UnsafeMutablePointer<FILE>!
+    
+    init?(path: String) {
+        self.path = path
+        
+        file = fopen(path, "r")
+        
+        guard file != nil else { return nil }
+        
+    }
+    
+    var nextLine: String? {
+        var line:UnsafeMutablePointer<CChar>? = nil
+        var linecap:Int = 0
+        defer { free(line) }
+        return getline(&line, &linecap, file) > 0 ? String(cString: line!) : nil
+    }
+    
+    deinit {
+        fclose(file)
+    }
+}
+
+extension LineReader: Sequence {
+    func  makeIterator() -> AnyIterator<String> {
+        return AnyIterator<String> {
+            return self.nextLine
+        }
+    }
+}
--- MentalHealthWeek/main.swift
+++ MentalHealthWeek/main.swift
@@ -8,41 +8,6 @@
 
 import Foundation
 
-/// Read text file line by line
-class LineReader {
-    let path: String
-    
-    fileprivate let file: UnsafeMutablePointer<FILE>!
-    
-    init?(path: String) {
-        self.path = path
-        
-        file = fopen(path, "r")
-        
-        guard file != nil else { return nil }
-        
-    }
-    
-    var nextLine: String? {
-        var line:UnsafeMutablePointer<CChar>? = nil
-        var linecap:Int = 0
-        defer { free(line) }
-        return getline(&line, &linecap, file) > 0 ? String(cString: line!) : nil
-    }
-    
-    deinit {
-        fclose(file)
-    }
-}
-
-extension LineReader: Sequence {
-    func  makeIterator() -> AnyIterator<String> {
-        return AnyIterator<String> {
-            return self.nextLine
-        }
-    }
-}
-
 // Read the text file (place in your home folder)
 // Path will probably be /Users/student/survey_response_sample.txt
 // Obtain the data file on Haiku, Day 37
