eea74f97b566a3960dfa457c7da6a3d2132c82e1
Mark McCutcheon <mark.mccutcheon29@gmail.com>
2017-01-15 16:50:57 -0500

    continued work on setting up necessary variables to handle parsing a line in the data.

--- MentalHealthWeek/main.swift
+++ MentalHealthWeek/main.swift
@@ -33,29 +33,42 @@ struct Student {
     
     
 }
-    struct Activity {
-        
-        var maxCount : Int
-        var currentCount : Int
-        var name : String
-        var attending = [String]()
-        init(maxCount : Int, currentCount : Int, name : String) {
-            self.maxCount = maxCount
-            self.currentCount = currentCount
-            self.name = name
-        }
-        
+struct Activity {
+    
+    var maxCount : Int
+    var currentCount : Int
+    var name : String
+    var attending = [String]()
+    init(maxCount : Int, currentCount : Int, name : String) {
+        self.maxCount = maxCount
+        self.currentCount = currentCount
+        self.name = name
     }
     
-    
-    
-    
-    
-    
-    
+}
+
+var students : [Student] = []
+
+var activities : [Activity] = [
+    Activity(maxCount: 160, currentCount: 0, name: "Breakfast"),
+    Activity(maxCount: 40, currentCount: 0, name: "Gym"),
+   Activity(maxCount: 20, currentCount: 0, name: "Yoga"),
+   Activity(maxCount: 30, currentCount: 0, name: "Academics"),
+    Activity(maxCount: 16, currentCount: 0, name: "Animals"),
+    Activity(maxCount: 160, currentCount: 0, name: "Relaxation"),
+    Activity(maxCount: 12, currentCount: 0, name: "Massage"),
+   Activity(maxCount: 500, currentCount: 0, name: "Sleep")
+]
+
+//MARK: Sorting Function
 
 
 
+//print(activities)
+
+
+
+//MARK: Read File
 
 // Read the text file (place in your home folder)
 // Path will probably be /Users/student/survey_response_sample.txt
@@ -73,17 +86,37 @@ var columnDescriptors : [String] = []
 for (number, line) in reader.enumerated() {
     
     // Look for first line and build an array of column descriptors
-    if number == 2 {
+    if number > 1 {
         
         // Get an array of all the information on the first line
         // "Explode" the string into an array of smaller strings using a comma as a delimiter
         columnDescriptors = line.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).components(separatedBy: ",")
         
+        var studentEmail = columnDescriptors[5]
+        var studentGrade = columnDescriptors[9]
+        var studentAdvisor = columnDescriptors[10]
+        var studentActivities : [String] = []
+        
+        
+        print(columnDescriptors)
         // Iterate over the array of column headers and print to the console
         for (column, descriptor) in columnDescriptors.enumerated() {
             //Columns apear to be accesed by int values, so a conditional checking column value is a good way to extract a specific descriptor
             
-            print("column \(column) : \(descriptor)")
+            if column == 9 {
+                
+                if Int(descriptor) == 9 {
+                
+                } else if Int(descriptor) == 10{
+                    
+                } else if Int(descriptor) == 11 {
+                    
+                } else {
+                    
+                }
+                
+            }
+            
             
         }
         
