f6d42770644524ee7a3c9255cd0d32ca335aa475
Mark McCutcheon <mark.mccutcheon29@gmail.com>
2017-01-15 17:45:16 -0500

    created a function that grabs all of the grade 9 activity rankings and puts them into an array for each day.

--- MentalHealthWeek/main.swift
+++ MentalHealthWeek/main.swift
@@ -49,6 +49,8 @@ struct Activity {
 
 var students : [Student] = []
 
+var descriptorLookup : [String] = []
+
 var activities : [Activity] = [
     Activity(maxCount: 160, currentCount: 0, name: "Breakfast"),
     Activity(maxCount: 40, currentCount: 0, name: "Gym"),
@@ -62,6 +64,91 @@ var activities : [Activity] = [
 
 //MARK: Sorting Function
 
+func getActivityName (whole: String) -> String  {
+    var switchNow = 0
+    var charSetup = [Character]()
+    var output = ""
+    for char in whole.characters {
+        if switchNow == 2 {
+            charSetup.append(char)
+            
+        }
+        
+        if char == "_" {
+            switchNow += 1
+        }
+    }
+    output = String(charSetup)
+    return output
+}
+
+func getActivityDay (whole: String) -> String  {
+    var charSetup = [Character]()
+    var output = ""
+    for char in whole.characters {
+        if char == "_" {
+            output = String(charSetup)
+            return output
+        }
+        charSetup.append(char)
+    }
+    return output
+}
+
+func getgrade9 (columnDescriptors : [String] ) {
+
+    var monday : [String : Int] = [
+    
+        "Breakfast" : Int(columnDescriptors[11])!,
+        "Gym" : Int(columnDescriptors[12])!,
+        "Yoga" : Int(columnDescriptors[13])!,
+        "Academics" : Int(columnDescriptors[14])!,
+        "Animals" : Int(columnDescriptors[15])!
+    ]
+    
+    var tuesday : [String : Int] = [
+        
+        "Breakfast" : Int(columnDescriptors[16])!,
+        "Gym" : Int(columnDescriptors[17])!,
+        "Yoga" : Int(columnDescriptors[18])!,
+        "Academics" : Int(columnDescriptors[19])!,
+        "Animals" : Int(columnDescriptors[20])!
+    ]
+    
+    var wednesday : [String : Int] = [
+        
+        "Breakfast" : Int(columnDescriptors[21])!,
+        "Gym" : Int(columnDescriptors[22])!,
+        "Yoga" : Int(columnDescriptors[23])!,
+        "Academics" : Int(columnDescriptors[24])!,
+        "Animals" : Int(columnDescriptors[25])!,
+        "Relaxation" : Int(columnDescriptors[26])!
+    ]
+    
+    var thursday : [String : Int] = [
+        
+        "Breakfast" : Int(columnDescriptors[28])!,
+        "Gym" : Int(columnDescriptors[29])!,
+        "Relaxation" : Int(columnDescriptors[30])!,
+        "Academics" : Int(columnDescriptors[31])!,
+        "Animals" : Int(columnDescriptors[32])!,
+        "Sleep" : Int(columnDescriptors[27])!
+    ]
+    
+    var friday : [String : Int] = [
+        
+        "Sleep" : Int(columnDescriptors[33])!,
+        "Gym" : Int(columnDescriptors[34])!,
+        "Relaxation" : Int(columnDescriptors[35])!,
+        "Academics" : Int(columnDescriptors[36])!,
+        "Animals" : Int(columnDescriptors[37])!,
+        "Maaage" : Int(columnDescriptors[38])!
+    ]
+    
+    
+    
+    
+}
 
 
 //print(activities)
@@ -86,7 +173,18 @@ var columnDescriptors : [String] = []
 for (number, line) in reader.enumerated() {
     
     // Look for first line and build an array of column descriptors
-    if number > 1 {
+    if number == 1 {
+        
+      columnDescriptors = line.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).components(separatedBy: ",")
+        
+        for (column, descriptor) in columnDescriptors.enumerated()
+        {
+            descriptorLookup.append(descriptor) // Build descriptor lookup table
+        }
+        
+        
+        
+    } else {
         
         // Get an array of all the information on the first line
         // "Explode" the string into an array of smaller strings using a comma as a delimiter
@@ -97,25 +195,18 @@ for (number, line) in reader.enumerated() {
         var studentAdvisor = columnDescriptors[10]
         var studentActivities : [String] = []
         
+        if let studentGradenum = Int(studentGrade) {
+            
+        
+            
+            
+        }
+        
         
         print(columnDescriptors)
         // Iterate over the array of column headers and print to the console
         for (column, descriptor) in columnDescriptors.enumerated() {
             //Columns apear to be accesed by int values, so a conditional checking column value is a good way to extract a specific descriptor
-            
-            if column == 9 {
-                
-                if Int(descriptor) == 9 {
-                
-                } else if Int(descriptor) == 10{
-                    
-                } else if Int(descriptor) == 11 {
-                    
-                } else {
-                    
-                }
-                
-            }
             
             
         }
